1be2c277a1e7740a7ba8601d9e9edfaa182de28f
luckwillbealwaysCrochet <crochetworkacc@yandex.ru>
2022-07-08 17:15:19 +0300

    Add index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.patch(
   PostController.update,
 );
 
-app.listen(process.env.Port || 4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
